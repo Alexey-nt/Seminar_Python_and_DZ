9f00f3b148f4b802a227e8a37a361a976c52da9f
Alexey <alexey-nt@mail.ru>
2023-02-23 12:09:13 +0500

    created table

--- lection_Pandas.py
+++ lection_Pandas.py
@@ -1,3 +1,4 @@
 import pandas as pd
-df = pd.read_csv('sample_data/california_housing_train.csv')
+
+df = pd.read_csv('C:\Users\alexe\OneDrive\Документы\Seminar_Python\california_housing_train.csv')
 df.head()
